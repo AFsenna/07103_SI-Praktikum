745289e0c0924b2d60f6edc06f936625143af182
AFsenna <Fgelicia@gmail.com>
2021-04-16 00:45:04 +0700

    7.6 - 7.7 PraktikumModel.php & 7.9 DaftarPrakModel.php

--- Model/PraktikumModel.php
+++ Model/PraktikumModel.php
@@ -1,4 +1,27 @@
 <?php
 class PraktikumModel
 {
+    /**
+     * Function get berfungsi untuk mengambil seluruh data dari database
+     */
+
+     public function get(){
+         $sql = "SELECT * FROM praktikum";
+         $query = koneksi()->query($sql);
+         $hasil = [];
+         while($data = $query->fetch_assoc()){
+             $hasil[] = $data;
+         }
+         return $hasil;
+     }
+
+     /**
+      * Function index berfungsi untuk mengatur tampilan awal
+      */
+
+      public function index(){
+          $data = $this->get();
+          extract($data);
+          require_once("View/praktikum/index.php");
+      }
 }
